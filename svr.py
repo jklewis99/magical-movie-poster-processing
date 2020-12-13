02cfec034228ceeb80fd03a69a5befb871956c31
Minnie Kim <minniekim00@hotmail.com>
2020-12-13 15:44:41 -0500

    Added regression line on plot

--- svr.py
+++ svr.py
@@ -37,6 +37,26 @@ def main():
 
         plt.figure()
         plt.scatter(preds, y_test, s = 1, marker = "o", facecolor = "none", edgecolor = "blue")
+
+        # Plot line from min point to max point
+        ax = plt.gca()
+        xlim = ax.get_xlim()
+        ylim = ax.get_ylim()
+
+        # slope of line
+        x1 = xlim[0]
+        y1 = ylim[0]
+        x2 = xlim[1]
+        y2 = ylim[1]
+        m = (y2 - y1) / (x2 - x1)
+
+        regression_line = []
+        for x in preds:
+            regression_line.append((m*x))
+
+        plt.plot(preds, regression_line, color = 'red', linewidth=1)
+
+
         plt.title('Actual Revenue vs Predicted Revenue (' + kernel + ")", fontsize = 14)
         plt.ylabel('Actual Revenue', fontsize = 12)
         plt.xlabel('Predicted Revenue', fontsize = 12)
